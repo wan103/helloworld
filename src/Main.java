55bd724448a182f4173ab1e6830259b3845e6dcf
wyj <wan103@sina.com>
2018-07-30 18:57:18 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("hihi");
-        System.out.println("hihi");        System.out.println("hihi");
+         System.out.println("22222");
     }
 }
