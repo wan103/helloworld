bfbd83e49caf3b2a9ddde940b26cfea90cc819d4
wan103 <wyjwan103301>
2018-07-30 19:00:12 +0800

    hi2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-         System.out.println("22222");
+         System.out.println("hhi");
     }
 }
